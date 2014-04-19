0dd86fb6119200ccad8ee70c43389f937c7f4116
rbaumbach <rbaumbach.github@gmail.com>
2014-04-18 21:22:36 -0700

    Add OvenSpec

--- Specs/OvenSpec.mm
+++ Specs/OvenSpec.mm
@@ -1,23 +1,19 @@
-#import "House.h"
+#import "Oven.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
 
 
-SPEC_BEGIN(HouseSpec)
+SPEC_BEGIN(OvenSpec)
 
-describe(@"House", ^{
-    __block House *house;
+describe(@"Oven", ^{
     __block Oven *oven;
     
     beforeEach(^{
         oven = [[Oven alloc] init];
-        house = [[House alloc] initWithOven:oven];
     });
     
     it(@"exists", ^{
-        house should_not be_nil;
+        oven should_not be_nil;
     });
 });
-
-SPEC_END
