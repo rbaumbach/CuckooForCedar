654fd9579d40d4311fde5bf4588f0dffcbd7af72
rbaumbach <rbaumbach.github@gmail.com>
2014-04-23 18:32:18 -0700

    Add failing test to SuperBurritoSpec

--- Specs/SuperBurritoSpec.mm
+++ Specs/SuperBurritoSpec.mm
@@ -16,6 +16,10 @@ describe(@"SuperBurrito", ^{
     it(@"creates a super burrito", ^{
         superBurrito should_not be_nil;
     });
+    
+    it(@"has correct super burrito ingredients", ^{
+        superBurrito.ingredients should equal(@[@"Tortilla", @"Carne Asada", @"Refried Beans", @"Spanish Rice", @"Salsa", @"Sour Cream", @"Guacamole", @"Cheese"]);
+    });
 });
 
 SPEC_END
